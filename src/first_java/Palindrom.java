dabd686024f4e2a4b2ad67a81210cf8c3a459074
MaB@2210 <mannbhatt.00@gmail.com>
2022-11-14 13:03:23 -0500

    Palindrom Done

--- src/first_java/Palindrom.java
+++ src/first_java/Palindrom.java
@@ -1,7 +1,33 @@
 package first_java;
 
+import java.util.Scanner;
+
 public class Palindrom {
     public static void main(String[] args) {
+
+        int rem, sum=0, temp, num;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Please Enter the Number => ");
+        num=sc.nextInt();
+
+        temp=num;
+
+        while(num>0)
+        {
+            rem=num%10;
+            sum=(sum*10)+rem;
+            num=num/10;
+
+        }
+        if(temp==sum){
+            System.out.println("Number is Palindrom");
+        }
+        else{
+            System.out.println("Number is NotPalindrom");
+        }
+
+
 
     }
 }
